00170c75812ecfe20540fc4c9213cdb182dfbe2b
rifqi <rifqiimtinan@gmail.com>
2023-12-21 16:53:10 +0700

    remove detail author

--- components/blog/authorCard.js
+++ components/blog/authorCard.js
@@ -24,19 +24,19 @@ export default function AuthorCard({ author }) {
         <div>
           <div className="mb-3">
             <h3 className="text-lg font-medium text-gray-800 dark:text-gray-300">
-              About {author.name}
+               {author.name}
             </h3>
           </div>
           <div>
             {author.bio && <PortableText value={author.bio} />}
           </div>
-          <div className="mt-3">
+          {/* <div className="mt-3">
             <Link
               href={`/author/${author.slug.current}`}
               className="bg-brand-secondary/20 rounded-full py-2 text-sm text-blue-600 dark:text-blue-500 ">
               View Profile
             </Link>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
