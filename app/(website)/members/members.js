9c04a9a9ab39e68e74285daa2efbfbeaf44bd6ab
rifqi <rifqiimtinan@gmail.com>
2023-12-21 15:53:20 +0700

    changes layout headers

--- app/(website)/members/members.js
+++ app/(website)/members/members.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function About({ authors, settings }) {
+  
   return (
     <Container>
       <h1 className="text-brand-primary mb-3 mt-2 text-center text-3xl font-semibold tracking-tight dark:text-white lg:text-4xl lg:leading-snug">
@@ -14,7 +15,7 @@ export default function About({ authors, settings }) {
       </div> */}
 
       <div className="mb-16 mt-6 grid grid-cols-3 gap-5 md:mb-32 md:mt-16 md:gap-16">
-        {authors.map(author => {
+        {authors.filter((data) => !data.name.includes(",")).map(author => {
           const imageProps = urlForImage(author?.image) || null;
           return (
             <>
